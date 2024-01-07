3b3ee2232d64386cb8f5945e4208e2fa33c5607e
DanielKuzn <kuznickidaniel@wp.pl>
2024-01-07 22:37:16 +0100

    Added loadData and selectOption methods

--- TaskManager/src/main/java/pl/coderslab/TaskManager.java
+++ TaskManager/src/main/java/pl/coderslab/TaskManager.java
@@ -1,4 +1,51 @@
 package pl.coderslab;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
 public class TaskManager {
+    static String[][] tasks;
+    public static void main(String[] args) {
+        loadData();
+        selectOption();
+    }
+    public static void loadData() {
+        Path inputPath = Paths.get("tasks.csv");
+        int lineNumber = 0;
+        try {
+            for (String line : Files.readAllLines(inputPath)) {
+                lineNumber++;
+            }
+            tasks = new String[lineNumber][];
+            lineNumber = 0;
+            for (String line : Files.readAllLines(inputPath)) {
+                tasks[lineNumber] = line.split(",");
+                lineNumber++;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public static void selectOption() {
+        Scanner scan = new Scanner(System.in);
+        String[] options = {"add", "remove", "list", "exit"};
+        String inputOption = "";
+        while (!inputOption.equals("exit")) {
+            System.out.println(ConsoleColors.BLUE + "Please select an option:");
+            for (String option : options) {
+                System.out.println(ConsoleColors.RESET + option);
+            }
+            inputOption = scan.next();
+            switch (inputOption) {
+                case "add" -> System.out.println("Add task"); //addTask();
+                case "remove" -> System.out.println("Remove task"); //remove();
+                case "list" -> System.out.println("List task"); //showTaskList();
+                case "exit" -> System.out.println("Exit program"); //exitProgram();
+                default -> System.out.println("Please select a correct option.");
+            }
+        }
+    }
 }
