88d0afb514095392d89eedeaf3e93b7fc73de46a
DanielKuzn <kuznickidaniel@wp.pl>
2024-01-10 00:16:58 +0100

    Added functions for removing and saving tasks

--- TaskManager/src/main/java/pl/coderslab/TaskManager.java
+++ TaskManager/src/main/java/pl/coderslab/TaskManager.java
@@ -1,9 +1,14 @@
 package pl.coderslab;
 
+import org.apache.commons.lang3.ArrayUtils;
+import org.apache.commons.lang3.math.NumberUtils;
+
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -42,9 +47,9 @@ public class TaskManager {
             inputOption = scan.next();
             switch (inputOption) {
                 case "add" -> addTask();
-                case "remove" -> System.out.println("Remove task"); //remove();
+                case "remove" -> removeTask();
                 case "list" -> showTaskList();
-                case "exit" -> System.out.println("Exit program"); //exitProgram();
+                case "exit" -> exitProgram();
                 default -> System.out.println("Please select a correct option.");
             }
         }
@@ -63,6 +68,39 @@ public class TaskManager {
     public static void showTaskList() {
         for (int i = 0; i < tasks.length; i++) {
             System.out.println(i + " : " + tasks[i][0] + " " + tasks[i][1] + " " + tasks[i][2]);
+        }
+    }
+    public static void removeTask() {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Please select number to remove:");
+        int taskNumber = -1;
+        while (taskNumber < 0) {
+            while (!scan.hasNextInt()) {
+                System.out.println("Incorrect argument passed. Please give number greater or equal 0:");
+                scan.next();
+            }
+            taskNumber = scan.nextInt();
+            if (taskNumber < 0) {
+                System.out.println("Incorrect argument passed. Please give number greater or equal 0:");
+            }
+        }
+        try {
+            tasks = ArrayUtils.remove(tasks, taskNumber);
+            System.out.println("Value was successfully deleted.");
+        } catch (ArrayIndexOutOfBoundsException e) {
+            e.printStackTrace();
+        }
+    }
+    public static void exitProgram() {
+        try (FileWriter writer = new FileWriter("tasks.csv", false)) {
+            for (String[] line : tasks) {
+                writer.append(line[0] + ",");
+                writer.append(line[1] + ",");
+                writer.append(line[2] + "\n");
+            }
+            System.out.println(ConsoleColors.RED + "Bye,  bye.");
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
